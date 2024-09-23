8e6dad211ce7e08a40d64c90ca71f70915ccb692
mahesh <maheshbemanapalli@gmail.com>
2024-09-23 18:13:00 +0530

    uplifted the turns information to app component from gameBoard component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Player from "./components/Player"
 import { useState } from "react"
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const initialGameBoard = [
+  [null,null,null],
+  [null,null,null],
+  [null,null,null]
+]
+
 function deriveActivePlayer(gameTurns)
 {
   let currentPlayer = 'X';
@@ -21,6 +27,16 @@ function App() {
   
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard
+
+    for(const turn of gameTurns)
+    {
+        const {square,player} = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
 
   function handleSelectSquare(rowIndex,colIndex) {
     //setActivePlayer((currentActivePlayer) => currentActivePlayer==='X'?'O':'X')
@@ -43,7 +59,7 @@ function App() {
           <Player initialName={"Player-1"} symbol={"X"} isActive={activePlayer==='X'}/>
           <Player initialName={"Player-2"} symbol={"O"} isActive={activePlayer==='O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
     </main>
