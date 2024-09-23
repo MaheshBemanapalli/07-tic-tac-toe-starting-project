f392d70e99322e596e649b6921739ae9ea7a8ed8
mahesh <maheshbemanapalli@gmail.com>
2024-09-23 15:12:24 +0530

    made changes to get the log using lifting up of state to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log";
 import Player from "./components/Player"
 import { useState } from "react"
 
@@ -6,9 +7,22 @@ function App() {
 
   //creating the state heare so that 2 components can share the state which are in this component
   const[activePlayer,setActivePlayer] = useState('X');
-  
-  function handleSelectSquare() {
+  const [gameTurns, setGameTurns] = useState([]);
+  function handleSelectSquare(rowIndex,colIndex) {
     setActivePlayer((currentActivePlayer) => currentActivePlayer==='X'?'O':'X')
+    setGameTurns(prevTurns=> {
+      let currentPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X')
+      {
+        currentPlayer = 'O';
+      } 
+      const updatedTurns = [
+        { square: { row:rowIndex, col: colIndex}, player: currentPlayer},
+        ...prevTurns,
+      ]
+
+      return updatedTurns;
+    })
   }
 
 
@@ -19,9 +33,9 @@ function App() {
           <Player initialName={"Player-1"} symbol={"X"} isActive={activePlayer==='X'}/>
           <Player initialName={"Player-2"} symbol={"O"} isActive={activePlayer==='O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
-      LOG
+      <Log turns={gameTurns}/>
     </main>
     
   )
