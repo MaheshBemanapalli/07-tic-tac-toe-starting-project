4ffae363a1e997686d53340991acc72bf6dee9b1
mahesh <maheshbemanapalli@gmail.com>
2024-11-05 15:43:42 +0530

    declaring the winner.

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,18 @@ function App() {
         gameBoard[row][col] = player;
     }
 
+    let winner;
+    for(const combinations of WINNING_COMBINATIONS)
+    {
+      const firstSquareSymbol = gameBoard[combinations[0].row][combinations[0].column];
+      const secondSquareSymbol = gameBoard[combinations[1].row][combinations[1].column];
+      const thirdSquareSymbol = gameBoard[combinations[2].row][combinations[2].column];
+
+        if( firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol)
+        {
+          winner = firstSquareSymbol;
+        }
+    }
   function handleSelectSquare(rowIndex,colIndex) {
     //setActivePlayer((currentActivePlayer) => currentActivePlayer==='X'?'O':'X')
     setGameTurns(prevTurns=> {
@@ -59,6 +71,7 @@ function App() {
           <Player initialName={"Player-1"} symbol={"X"} isActive={activePlayer==='X'}/>
           <Player initialName={"Player-2"} symbol={"O"} isActive={activePlayer==='O'}/>
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
